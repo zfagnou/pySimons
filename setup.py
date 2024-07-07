b510f17374ee7e8de7d8c208cd59a3fe0168fe6e
Zachary <zfagnou3@gmail.com>
2024-07-07 15:32:08 -0400

    changing file name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PyQuant',
+    name='PySimonms',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'numpy', 'pandas'
+        'numpy', 'pandas','pyfinance'
     ],
     entry_points={
         'console_scripts': [
